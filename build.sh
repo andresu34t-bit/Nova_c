795b15ad1a53af0fead776838221acf0418b1321
Nova Capital <admin@novacapital.com>
2026-05-30 19:55:57 -0400

    fix: build.sh improvements

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ echo "==> Collecting static files..."
 python manage.py collectstatic --no-input
 
 echo "==> Running migrations..."
-python manage.py migrate
+python manage.py migrate --no-input
 
 echo "==> Build complete!"
